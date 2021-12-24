63ac873e5f6b0a79b0c1a5b842d9f391986fdfe3
Eder Diego de Sousa <eder.diego@foursys.com.br>
2021-12-24 16:43:09 -0300

    ex 2609

--- uri2609/src/main/java/br/dev/diego/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/br/dev/diego/uri2609/Uri2609Application.java
@@ -1,6 +1,10 @@
 package br.dev.diego.uri2609;
 
+import br.dev.diego.uri2609.dto.CategorySumDTO;
+import br.dev.diego.uri2609.projections.CategorySumProjection;
 import br.dev.diego.uri2609.repositories.CategoryRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +22,15 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		List<CategorySumProjection> list = repository.search();
+		List<CategorySumDTO> listDTO = list.stream().map(categorySumProjection -> new CategorySumDTO(categorySumProjection)).collect(Collectors.toList());
+		System.out.println("NATIVE QUERY");
+		listDTO.forEach(System.out::println);
+
+		List<CategorySumDTO> listDTO1 = repository.search1();
+		System.out.println("JPQL QUERY");
+		listDTO1.forEach(System.out::println);
 
 	}
 }
--- uri2609/src/main/java/br/dev/diego/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/br/dev/diego/uri2609/repositories/CategoryRepository.java
@@ -1,8 +1,23 @@
 package br.dev.diego.uri2609.repositories;
 
+import br.dev.diego.uri2609.dto.CategorySumDTO;
 import br.dev.diego.uri2609.entities.Category;
+import br.dev.diego.uri2609.projections.CategorySumProjection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+  @Query(nativeQuery = true, value = "select c.name, sum(p.amount) "
+                                      + "from categories c "
+                                      + "inner join products p ON c.id = p.id_categories "
+                                      + "group by c.name")
+  List<CategorySumProjection> search();
+
+  @Query("select new br.dev.diego.uri2609.dto.CategorySumDTO(p.category.name, sum(p.amount)) "
+      + "from Product p "
+      + "group by p.category.name")
+  List<CategorySumDTO> search1();
 
 }
